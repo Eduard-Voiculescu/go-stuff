e4c6f071458de9421aaa615e5f51bcf83137af87
Eduard-Voiculescu <eduardvoiculescu95@gmail.com>
2022-01-23 19:55:12 -0500

    Adding mocking chapter

---
+++ countdown/countdown.go
@@ -0,0 +1,32 @@
+package countdown
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finaleWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finaleWord)
+}
