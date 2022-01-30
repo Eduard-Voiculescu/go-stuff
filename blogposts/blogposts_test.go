e0f9d04234f6bf80d7c7a3f795570ea40d2f020d
Eduard-Voiculescu <eduardvoiculescu95@gmail.com>
2022-01-30 17:29:41 -0500

    Adding blogposts, io file stuff

---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,65 @@
+package blogposts_test
+
+import (
+	"errors"
+	blogposts "github.com/eduardvoiculescu/blogposts"
+	"io/fs"
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
+
+func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
+	fileSystem := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fileSystem)
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fileSystem) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fileSystem))
+	}
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
---
+++ blogposts/go.mod
@@ -0,0 +1,3 @@
+module github.com/eduardvoiculescu/blogposts
+
+go 1.17
